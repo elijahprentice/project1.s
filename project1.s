bec90ed9fc7554873233bb100d6b7885b849bcd5
elijahprentice <prentee1213@gmail.com>
2019-10-28 21:41:19 -0400

    t registers, add onto sum

--- project1.s
+++ project1.s
@@ -5,13 +5,14 @@ main:
     li $a1,8                    #read 8 characters (including null character)
     syscall                     #execute code 8 to read the string
 
-    addu $s0,$s0,$zero          #loop increment
-    addu $s1,$s1,$zero          #hold sum
-    Loop:   bne $s0,6,Exit
-        addi $s0,$s0,1
+    addu $t0,$t0,$zero          #loop increment
+    addu $t1,$t1,$zero          #hold sum
+    Loop:   bne $t0,6,Exit
+        addu $t1,$a0,$t0        #the $t0 place in $a0 is added to $t1
+        addi $t0,$t0,1          #increment $t0 by 1
         j Loop
     Exit:   li $v0,1            #call code 1 (print_int)
-    move $a0,$s1                #$a0 = address of string
+    la $a0,$t1                  #$a0 = address of integer
     syscall                     #execute code 1 to print the integer
 
     li $v0,10                   #call code 10 (exit)
