49c1aa3ddfd1c3fe83afaa8bbf242d65b97d7a7c
elijahprentice <prentee1213@gmail.com>
2019-10-30 20:17:37 -0400

    load first character of string

--- project1.s
+++ project1.s
@@ -5,6 +5,11 @@ main:
     li $a1,11                   #read 11 characters (including null character)
     syscall                     #execute code 8 to read the string
 
+    la $a1,str
+    add $t0,$t0,$zero
+    addu $a1,$a1,$t0
+    lbu $a0,($a1)
+
     li $v0,10                   #call code 10 (exit)
     syscall                     #execute code 10 to exit
 
