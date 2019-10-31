cc8a8c6f5f7e5bfb59e5846c470733bba7993662
elijahprentice <prentee1213@gmail.com>
2019-10-31 09:29:21 -0400

    clear registers, j to lowercase ascii calculation

--- project1.s
+++ project1.s
@@ -8,12 +8,12 @@ main:
     la $a1,str                  #$a1 points to str
     add $t0,$t0,$a1             #$t0 holds string
 
-    addi $t1,$t1,113
     addi $t2,$t2,87
-
-    lbu $a0,($t0)               #load ascii value of [offset]($t0) to $a0
-    bgt $a0,$t1,OutRange
-        subu $a0,$a0,$t2
+    lbu $a0,($t0)               #load ascii value of 0($t0) to $a0
+    bgt $a0,113,OutRange        #if $a0 is greater than 113, jump to OutRange
+        bgt $a0,97,Lower       #if $a0 is greater than 97, jump to Lower
+        j OutRange
+        Lower:subu $a0,$a0,$t2
         j InRange
     OutRange:
         li $a0,0
