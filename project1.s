58b2e7c58b4314e506c26a7088d1c18864685eab
elijahprentice <prentee1213@gmail.com>
2019-10-30 20:57:37 -0400

    testing lbu and print

--- project1.s
+++ project1.s
@@ -9,14 +9,9 @@ main:
     add $t0,$t0,$zero           #x character in string
     addu $a1,$a1,$t0            #point to x character of string in $a1
     add $t1,$t1,$a1             #add ascii value of character to $t1
-    lbu $a0,($t1)               #load $t1 to $a0
+    lbu $a0,4($t1)               #load $t1 to $a0
     li $v0,1
     syscall
-
-    la $a1,str
-    addi $t0,$t0,1
-    addu $a1,$a1,$t0
-    add $t1,$t1,$a1
 
     li $v0,10                   #call code 10 (exit)
     syscall                     #execute code 10 to exit
