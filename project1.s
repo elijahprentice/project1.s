5c874a5346ef96d8498881f9cb6723b765755089
elijahprentice <prentee1213@gmail.com>
2019-10-30 20:00:22 -0400

    fit string length of assignment

--- project1.s
+++ project1.s
@@ -2,7 +2,7 @@
 main:
     li $v0,8                    #call code 8 (read_string)
     la $a0,str                  #$a0 points to the string
-    li $a1,8                    #read 8 characters (including null character)
+    li $a1,11                   #read 11 characters (including null character)
     syscall                     #execute code 8 to read the string
 
     li $v0,10                   #call code 10 (exit)
