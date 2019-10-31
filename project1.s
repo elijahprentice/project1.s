ac753fadbe7c7b5b789d03c4855f5ace0dbcb3b4
elijahprentice <prentee1213@gmail.com>
2019-10-31 15:03:02 -0400

    if 81 >= $a2 >= 65, subtract 55

--- project1.s
+++ project1.s
@@ -145,12 +145,16 @@ main:
     Done6:  addu $a0,$a0,$a2
 
     lbu $a2,7($t0)
+        bgt $a2,81,Out7
+            bg $a2,65,Up7
             bgt $a2,57,Out7
                 bge $a2,48,Num7
                 j Out7
                 Num7: subu $a2,$a2,$t3
                 j Done7
+            j Out7
             Up7: subu $a2,$a2,$t1
+            j Done7
         Low7: subu $a2,$a2,$t2
     Out7:    li $a2,0
     Done7:  addu $a0,$a0,$a2
