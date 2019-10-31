ab3ba07ec76d5534fe0b1e9799b5d3ad07811878
elijahprentice <prentee1213@gmail.com>
2019-10-31 13:30:09 -0400

    if $a2 >= 97, jump to Low2

--- project1.s
+++ project1.s
@@ -64,7 +64,11 @@ main:
 
     lbu $a2,2($t0)
     bgt $a2,113,Out2
+        bge $a2,97,Low2
+        
+        j Out2
         Low2: subu $a2,$a2,$t2
+        j Done2
     Out2:   li $a2,0
     Done2:  addu $a0,$a0,$a2
 
